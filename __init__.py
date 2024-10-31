16b6b7fdff4da15e27a86b20a498e70ac5fd19f4
Mathias <mathias@does.kr>
2024-10-31 12:23:00 +0900

    made title more clear

--- __init__.py
+++ __init__.py
@@ -7,5 +7,5 @@ NODE_CLASS_MAPPINGS = {
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DOES_custom_nodes_SendImageTypeWebSocket": "Send Image and Type(WebSocket, Base64)",
-    "DOES_custom_nodes_SendStatusMessageWebSocket": "Send Message and Type(WebSocket)",
+    "DOES_custom_nodes_SendStatusMessageWebSocket": "Send Message(WebSocket)",
 }
